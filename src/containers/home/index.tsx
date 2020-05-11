2a69f14212a408d6c8b272b1e1da46008ec522ea
Gary Pan <pangary51@gmail.com>
2020-05-10 20:58:41 -0700

    Add XState, update readMe, changed tab title

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import {Column} from "../../components/flexbox/Column";
 import {Header} from "../../components/text/Header";
-import {t} from "../../strings/i18n";
-import {Row} from "../../components/flexbox/Row";
-import {Paragraph} from "../../components/text/Paragraph";
 import styled from "styled-components";
+import {Machine} from "xstate";
+import {useMachine} from "@xstate/react/lib";
+import {t} from "../../strings/i18n";
 
 const RootColumn = styled(Column)`
     margin: 16px
 `
+const HOME_MACHINE_KEYS = {
+    STATES: {
+        SPLASH_LOADING: "splashLoading",
+        IDLE: "idle",
+        PROJECT_LOADING: "projectLoading",
+    },
+    ACTIONS: {
+        DISMISS_SPLASH: "DISMISS_SPLASH",
+        LOAD_PROJECT: "LOAD_PROJECT",
+    }
+}
 
-// #paper-row-container {
-//     display:inline-block
-// }
+export const homeMachine = Machine({
+    id: "home",
+    initial: HOME_MACHINE_KEYS.STATES.SPLASH_LOADING,
+    states: {
+        splashLoading: {
+            on: { DISMISS_SPLASH: HOME_MACHINE_KEYS.STATES.IDLE }
+        },
+        idle: {
+            on: { LOAD_PROJECT: HOME_MACHINE_KEYS.STATES.PROJECT_LOADING }
+        },
+        projectLoading: {
+
+        }
+    }
+})
 
 export const Home = () => {
+    const [currentState, sendState] = useMachine(homeMachine)
+
     return (
         <RootColumn>
             <Header type={"large"}>Coming Soon</Header>
