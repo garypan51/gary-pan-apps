39ab1a2c3c916733172320472d5483a7971a9622
Gary Pan <pangary51@gmail.com>
2020-05-25 16:04:24 -0700

    Update typescript

--- src/components/text/Header.tsx
+++ src/components/text/Header.tsx
@@ -1,23 +1,25 @@
+import React, {ReactNode} from "react"
 import styled from "styled-components"
 import {Theme} from "../../resources/Theme"
 
 interface IProps {
-    theme: Theme
+    theme?: Theme
     type?: "normal" | "large"
     fontSize?: string
     textColor?: string
     margin?: string
+    children: ReactNode
 }
 
-export const Header = styled.h1`
+export const StyledHeader = styled.h1`
     font-family: Muli, sans-serif;
     font-size: ${(props: IProps) => props.fontSize ?? "30px"};
-    color: ${(props: IProps) => props.textColor ?? props.theme.textColor};
+    color: ${(props: IProps) => props.textColor ?? props.theme?.textColor};
     text-align: center;
     padding: 0;
     margin:${(props: IProps) => props.margin ?? "0"};
 `
 
-Header.defaultProps = {
-    type: "normal"
+export const Header = (props: IProps) => {
+    return <StyledHeader {...props}/>
 }
--- src/components/text/Paragraph.tsx
+++ src/components/text/Paragraph.tsx
@@ -1,18 +1,24 @@
+import React, {ReactNode} from "react"
 import styled from "styled-components"
 import {Theme} from "../../resources/Theme"
 
 interface IProps {
-    theme: Theme
+    theme?: Theme
     fontSize?: string
     textColor?: string
     margin?: string
+    children: ReactNode
 }
 
-export const Paragraph = styled.p`
+const StyledParagraph = styled.p`
     font-family:Muli, sans-serif;
     font-size:${(props: IProps) => props.fontSize ?? "14px"};
-    color:${(props: IProps) => props.textColor ?? props.theme.textColor};
+    color:${(props: IProps) => props.textColor ?? props.theme?.textColor};
     padding:0;
     line-height:40px;
     margin:${(props: IProps) => props.margin ?? "0"};
 `
+
+export const Paragraph = (props: IProps) => {
+    return <StyledParagraph {...props}/>
+}
